3ab2b4439adc65af386a13b95d030376691325f5
Timo Tijhof <krinklemail@gmail.com>
2013-09-20 00:54:05 +0200

    Re-add version_compare (>= 1.22wmf18)
    Follows-up 98cf7e35e.
    
    Change-Id: I07a63effb5cdb14e48c6b69780fe71dc24f17fd9

--- TemplateData.php
+++ TemplateData.php
@@ -6,6 +6,11 @@
  * @ingroup Extensions
  */
 
+if ( version_compare( $wgVersion, '1.22wmf18', '<' ) ) {
+	echo "Extension:TemplateData requires MediaWiki 1.22 or higher.\n";
+	exit( 1 );
+}
+
 $wgExtensionCredits['parserhook'][] = array(
 	'path'          => __FILE__,
 	'name'          => 'TemplateData',
