479f887af9ae066ae82d46cfeff4b83bcb13d380
Ed Sanders <esanders@wikimedia.org>
2016-05-24 15:25:25 +0100

    Update stylelint-config version
    Change-Id: Ib2bfe549ff3c054c4363abbbc3ac78f4b6c6b51c

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,11 +8,11 @@
 module.exports = function ( grunt ) {
 	var conf = grunt.file.readJSON( 'extension.json' );
 
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
-	grunt.loadNpmTasks( 'grunt-jsonlint' );
-	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-jscs' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
