88f31acbd7f251a54b99c15f0ad14857a9e12fbc
Umherirrender <umherirrender_de.wp@web.de>
2020-12-13 00:27:07 +0100

    build: Remove unneeded phan suppression
    Possibly not needed after core change
    I40a305b5e49d67b3f6d7737c0846b93a80b78fa9
    
    Change-Id: Ided42bd763718f0393a874bbadb0b682b4bb9043

--- includes/api/ApiTemplateData.php
+++ includes/api/ApiTemplateData.php
@@ -111,7 +111,6 @@ class ApiTemplateData extends ApiBase {
 				$status = $tdb->getStatus();
 
 				if ( !$status->isOK() ) {
-					// @phan-suppress-next-line SecurityCheck-DoubleEscaped
 					$this->dieWithError(
 						[ 'apierror-templatedata-corrupt', intval( $row->pp_page ), $status->getMessage() ]
 					);
