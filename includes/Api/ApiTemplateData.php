1e0cecd49441ddbdb34829a39d2740205479e262
EpicPupper <wikimedia.dch34@aleeas.com>
2023-01-15 22:22:43 +0000

    Improve API examples
    Bug: T271040
    Change-Id: I5b9bbc932d887086d647ce89fb77d192a896d32d

--- includes/Api/ApiTemplateData.php
+++ includes/Api/ApiTemplateData.php
@@ -271,9 +271,9 @@ class ApiTemplateData extends ApiBase {
 	 */
 	protected function getExamplesMessages() {
 		return [
-			'action=templatedata&titles=Template:Stub|Template:Example&includeMissingTitles=1'
+			'action=templatedata&titles=Template:Foobar&includeMissingTitles=1'
 				=> 'apihelp-templatedata-example-1',
-			'action=templatedata&titles=Template:Stub|Template:Example'
+			'action=templatedata&titles=Template:Phabricator'
 				=> 'apihelp-templatedata-example-2',
 		];
 	}
