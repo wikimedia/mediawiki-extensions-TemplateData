0fc4a34b53b413413b5c0a733d0c00566ffeff9b
TheresNoTime <theresnotime@wikimedia.org>
2025-02-10 15:30:58 +0000

    SpecialTemplateSearch: Add Special page
    Add a special page for testing the template search widget
    Add barebones phpunit test for special page
    
    Bug: T385034
    Depends-On: Ia23107ccbddb34bc33982632214d1b0e0fc88e5c
    Change-Id: Ie9a575e392ea1b3a10cbd65c12e1a826ac00971b

---
+++ includes/Special/SpecialTemplateSearch.php
@@ -0,0 +1,30 @@
+<?php
+namespace MediaWiki\Extension\TemplateData\Special;
+
+use MediaWiki\Html\Html;
+use MediaWiki\SpecialPage\SpecialPage;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class SpecialTemplateSearch extends SpecialPage {
+
+	public function __construct() {
+		parent::__construct( 'TemplateSearch', '', false );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function execute( $par ) {
+		$output = $this->getOutput();
+		$this->setHeaders();
+		if ( $this->getConfig()->get( 'TemplateDataEnableDiscovery' ) ) {
+			$output->addHTML( Html::element( 'div', [ 'id' => 'ext-TemplateData-SpecialTemplateSearch' ] ) );
+			$output->addModules( 'ext.templateData.templateDiscovery' );
+		} else {
+			$output->addWikiTextAsInterface( $this->msg( 'templatedata-template-discovery-disabled' )->text() );
+		}
+	}
+
+}
--- modules/ext.templateData.templateDiscovery/init.js
+++ modules/ext.templateData.templateDiscovery/init.js
@@ -2,3 +2,15 @@ mw.templateData = {
 	config: require( './config.json' ),
 	TemplateSearchLayout: require( './TemplateSearchLayout.js' )
 };
+
+// If we're on the TemplateSearch special page
+if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'TemplateSearch' ) {
+	const specialTemplateSearch = document.getElementById( 'ext-TemplateData-SpecialTemplateSearch' );
+	if ( specialTemplateSearch ) {
+		const config = {
+			expanded: false
+		};
+		const searchForm = new mw.templateData.TemplateSearchLayout( config );
+		specialTemplateSearch.append( searchForm.$element[ 0 ] );
+	}
+}
