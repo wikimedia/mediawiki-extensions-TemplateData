7d076c069633c0e9fcf5ca6e8648edb95d057d02
Sam Wilson <sam@samwilson.id.au>
2025-02-25 15:08:44 +0800

    Template Discovery: Add favourite icon to search results
    Add a favourite button with bookmark icon to the end of each search result row in the combobox dropdown. The button does not yet do anything.
    
    Bug: T377455
    Change-Id: I9f1d4f59baa1f917baf4ba9e57fb1a4fcd476b02

--- modules/ext.templateData.templateDiscovery/SearchResult.js
+++ modules/ext.templateData.templateDiscovery/SearchResult.js
@@ -5,15 +5,20 @@
  * @constructor
  * @param {Object} config
  * @param {jQuery|string} [config.description=''] Search result description
+ * @param {boolean} [config.isFavourite=false] Whether this template has been favourited
  * @param {string} [config.data.redirecttitle] Page title for the "redirected from" message
  */
 function SearchResult( config ) {
+	config = Object.assign( {
+		classes: [ 'ext-templatedata-SearchResult' ],
+		isFavourite: false
+	}, config );
 	SearchResult.super.call( this, config );
 
 	if ( config.data.redirecttitle ) {
 		const redirecttitle = new mw.Title( config.data.redirecttitle )
 			.getRelativeText( mw.config.get( 'wgNamespaceIds' ).template );
-		$( '<div>' )
+		$( '<span>' )
 			.addClass( 'ext-templatedata-search-redirectedfrom' )
 			.text( mw.msg( 'redirectedfrom', redirecttitle ) )
 			.appendTo( this.$element );
@@ -23,8 +28,29 @@ function SearchResult( config ) {
 		.addClass( 'ext-templatedata-search-description' )
 		.append( $( '<bdi>' ).text( config.description || '' ) )
 		.appendTo( this.$element );
+
+	// Add a wrapper element so that the button and the other elements are in separate containers.
+	const $wrap = $( '<span>' );
+	$wrap.append( this.$element.contents() );
+	this.$element.append( $wrap );
+
+	this.isFavourite = config.isFavourite;
+
+	this.favouriteButton = new OO.ui.ButtonInputWidget( {
+		icon: 'bookmarkOutline',
+		framed: false,
+		invisibleLabel: true,
+		type: 'button'
+	} );
+	this.favouriteButton.connect( this, { click: this.clickFavourite } );
+	this.$element.append( this.favouriteButton.$element );
 }
 
 OO.inheritClass( SearchResult, OO.ui.MenuOptionWidget );
+
+SearchResult.prototype.clickFavourite = function () {
+	this.isFavourite = !this.isFavourite;
+	this.favouriteButton.setIcon( this.isFavourite ? 'bookmark' : 'bookmarkOutline' );
+};
 
 module.exports = SearchResult;
