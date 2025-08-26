0feb8948bd261ba9dcb7bc71cf0ccdb0583e9623
Sam Wilson <sam@samwilson.id.au>
2025-08-26 17:02:35 +0800

    Template category browser: look up root cat from WikibaseClient
    If WikibaseClient is available, and a (new) config var has been
    set, try to look up the current site's sitelink in order to get
    the title of the root category to be used for template browsing.
    
    Bug: T398292
    Change-Id: I2da1557362aa2cd6de7f03d319f6cf1df8ab47c2

--- .phan/config.php
+++ .phan/config.php
@@ -13,6 +13,7 @@ $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'],
 	[
 		'../../extensions/CommunityConfiguration',
+		'../../extensions/Wikibase',
 	]
 );
 
@@ -20,6 +21,7 @@ $cfg['exclude_analysis_directory_list'] = array_merge(
 	$cfg['exclude_analysis_directory_list'],
 	[
 		'../../extensions/CommunityConfiguration',
+		'../../extensions/Wikibase',
 	]
 );
 
